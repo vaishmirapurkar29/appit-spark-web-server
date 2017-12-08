37624e44b5947ff3d5e08f156bbed79cc6a18ccb
Shivendra96 <shivendra221@gmail.com>
2017-12-08 10:37:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -41,21 +41,24 @@ app.post('/login',urlencodedParser,function(req,res){
         if(check===pw)
         {
           console.log(result);
-          res.render('login-success',{data: req.body})
+          res.writeHead(200, {'Content-Type': 'application/json'});
           var myobj={
             uid: result[0].userid,
             uname:result[0].uname,
             dob:result[0].dob ,
             pw: result[0].pass,
-            email:result[0].email
+            email: result[0].email
           };
           res.end(JSON.stringify(myobj));
+          //res.render('login-success',{data: req.body})
           console.log(myobj)
         }
         else {
+          var myobj2={status:'FALSE'};
           console.log('No');
           console.log(check);
-          res.render('login-noemail');
+          res.end(JSON.stringify(myobj2));
+          //res.render('login-noemail');
         }
       }
     }
