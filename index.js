e97259a2ca079252c89e3a0be1d7dc3b1fa0fb93
Shivendra96 <shivendra221@gmail.com>
2017-12-14 14:20:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/login',urlencodedParser,function(req,res){
         var check=[];
         check=result[0].password;
         console.log(check);
-        if(check.equals(pw)==true)
+        if(check==pw)
         {
           res.writeHead(200, {'Content-Type': 'application/json'});
           var myobj={
