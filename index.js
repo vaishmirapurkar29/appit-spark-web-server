b698ae17256fcd7f8e7a1d202ecf47a8bc63f76a
Shivendra96 <shivendra221@gmail.com>
2018-01-18 19:55:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ app.get('/sign-up',function(req,res){
 app.get('/login',function(req,res){
   res.render('login',{qs: req.query})
 });
+app.get('/business',function(req,res){
+  res.render('business',{qs: req.query})
+});
 app.post('/login',urlencodedParser,function(req,res){
   console.log(req.body);
   var pw=req.body.pw;
@@ -50,10 +53,8 @@ app.post('/login',urlencodedParser,function(req,res){
               dob:result[0].dob
               });
             var my_count;
-            //my_count=[];
             con.query("SELECT COUNT(*) AS namesCount FROM businesses", function (err, rows, fields) {
             if (err) throw err;
-            //console.log(rows[0].namesCount);
             my_count=rows[0].namesCount;
             });
             var myobj_business; var ar;
@@ -64,12 +65,7 @@ app.post('/login',urlencodedParser,function(req,res){
               myobj_business.push({
                 business_id:result[i].business_id,
                 name:result[i].name,
-                //address:result[i].address,
-                type:result[i].type,
-                //phone:result[i].phone,
-                //open_hours:result[i].open_hours,
-                //number_of_reviews:result[i].number_of_reviews,
-                //average_rating:result[i].average_rating
+                type:result[i].type
               });
               }
               res.writeHead(200, {'Content-Type': 'application/json'});
@@ -80,7 +76,7 @@ app.post('/login',urlencodedParser,function(req,res){
         }
         else {
           res.writeHead(200, {'Content-Type': 'application/json'});
-          var myobj2={status:'FALSE'};
+          var myobj2={credentials:'FALSE'};
           console.log('No');
           console.log(check);
           res.end(JSON.stringify(myobj2));
@@ -90,7 +86,42 @@ app.post('/login',urlencodedParser,function(req,res){
     }
   });
 });
-app.get('/profile/:name', function(req, res){
-  res.render('profile');
+app.post('/business',urlencodedParser,function(req,res){
+  console.log(req.body);
+  var myobj_reviews=[];
+  var business_id=req.body.business_id;
+  var bid=business_id;
+  var sql = 'SELECT * FROM businesses WHERE business_id = ?';
+  con.query(sql, [bid], function (err, result) {
+    if (err) throw err;
+    var arr={business_id:result[0].business_id,
+    name:result[0].name,
+    address:result[0].address,
+    type:result[0].type,
+    phone:result[0].phone,
+    open_hours:result[0].open_hours,
+    number_of_reviews:result[0].number_of_reviews,
+    average_rating:result[0].average_rating };
+    var sql_rev = 'SELECT * FROM reviews WHERE business_id = ?';
+    con.query(sql_rev, [bid], function (err_rev, result_rev) {
+      if (err_rev) throw err_rev;
+    myobj_reviews.push({
+      review_id:result_rev[0].review_id,
+      lighting:result_rev[0].lighting,
+      audio:result_rev[0].audio,
+      decoration:result_rev[0].decoration,
+      staff:result_rev[0].staff,
+      comment:result_rev[0].comment,
+      average:result_rev[0].average,
+      user_id:result_rev[0].user_id,
+      business_id:result_rev[0].business_id
+    });
+    console.log(myobj_reviews);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+      var bus_in={'business':arr,'reviews':myobj_reviews};
+      res.end(JSON.stringify(bus_in));
+  });
+
+  });
 });
 app.listen(3000);
