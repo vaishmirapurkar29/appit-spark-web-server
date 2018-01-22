bd6f2b1a6804f9a905454a053fc2b5e03c74e428
Shivendra96 <shivendra221@gmail.com>
2018-01-22 16:44:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var express=require('express');
 var app=express();
 var bodyParser=require('body-parser');
 var urlencodedParser=bodyParser.urlencoded({extended: false});
+
+// import the async package to deal with asynchronous db interactions
+var async = require('async');
+
+// parse application/json
+app.use(bodyParser.json());
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "team",
@@ -91,6 +98,7 @@ app.post('/business',urlencodedParser,function(req,res){
   var myobj_reviews=[];
   var business_id=req.body.business_id;
   var bid=business_id;
+  var count_rev;
   var sql = 'SELECT * FROM businesses WHERE business_id = ?';
   con.query(sql, [bid], function (err, result) {
     if (err) throw err;
@@ -105,22 +113,114 @@ app.post('/business',urlencodedParser,function(req,res){
     var sql_rev = 'SELECT * FROM reviews WHERE business_id = ?';
     con.query(sql_rev, [bid], function (err_rev, result_rev) {
       if (err_rev) throw err_rev;
+    var rev_count='SELECT COUNT(*) AS reviewCount FROM reviews WHERE business_id = ?';
+    con.query(rev_count, [bid], function (err_count, result_count) {
+      if (err_count) throw err_count;
+      if (err) throw err;
+      count_rev=result_count[0].reviewCount;
+      for (i = 0; i < count_rev; i++) {
     myobj_reviews.push({
-      review_id:result_rev[0].review_id,
-      lighting:result_rev[0].lighting,
-      audio:result_rev[0].audio,
-      decoration:result_rev[0].decoration,
-      staff:result_rev[0].staff,
-      comment:result_rev[0].comment,
-      average:result_rev[0].average,
-      user_id:result_rev[0].user_id,
-      business_id:result_rev[0].business_id
+      review_id:result_rev[i].review_id,
+      lighting:result_rev[i].lighting,
+      audio:result_rev[i].audio,
+      decoration:result_rev[i].decoration,
+      staff:result_rev[i].staff,
+      comment:result_rev[i].comment,
+      average:result_rev[i].average,
+      user_id:result_rev[i].user_id,
+      business_id:result_rev[i].business_id
     });
+  }
     console.log(myobj_reviews);
     res.writeHead(200, {'Content-Type': 'application/json'});
       var bus_in={'business':arr,'reviews':myobj_reviews};
       res.end(JSON.stringify(bus_in));
   });
   });
+  });
 });
-app.listen(3000);
+
+
+/*****USERS CREATE REVIEWS*****/
+// handle a POST request at the route that let users create reviews
+app.post('/users/:userId/reviews', function(request, response) {
+  // retrieve the user's id from the url parameter
+  var userId = Number(request.params.userId);
+
+  // other info will be retrieved from the body of the request
+  var businessId = request.body.businessId;
+  var lighting = request.body.lighting;
+  var audio = request.body.audio;
+  var decoration = request.body.decoration;
+  var staff = request.body.staff;
+  var comment = request.body.comment;
+  var average = (lighting + audio + decoration + staff) / 4;
+
+  var numberOfReviews;
+  var averageRating;
+
+  var reply = {
+    status: true
+  };
+
+  async.series([
+    // let user insert a review first
+    function(callback) {
+      // put the values of the variables into the SQL statement using parameter substitution
+      var insertReview =  'INSERT INTO reviews(lighting, audio, decoration, staff, comment, average, user_id, business_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
+      // since we have multiple subsitutions, use an array
+      con.query(insertReview, [lighting, audio, decoration, staff, comment, average, userId, businessId], function(err, result) {
+        if(err) {
+          reply.status = false;
+          response.send(reply);
+          return callback(err);
+        }
+        console.log("userId " + userId + " inserted 1 review");
+        callback();
+      });
+    },
+
+    // then re-calculate the number of reviews and average rating in the businesses table
+    function(callback) {
+      var queryInfo = 'SELECT number_of_reviews, average_rating FROM businesses WHERE business_id = ?';
+      con.query(queryInfo, businessId, function(err, result) {
+        if(err) {
+          reply.status = false;
+          response.send(reply);
+          return callback(err);
+        }
+        // "result" is an array containing each row as an object
+        numberOfReviews = result[0].number_of_reviews;
+        averageRating = result[0].average_rating;
+        console.log("current number of reviews of business #" + businessId + ": " + numberOfReviews);
+        console.log("current average rating of business #" + businessId + ": " + averageRating);
+        averageRating = ((averageRating * numberOfReviews) + average) / (numberOfReviews + 1);
+        numberOfReviews++;
+        console.log("after update, number of reviews: " + numberOfReviews);
+        console.log("after update, average rating: " + averageRating);
+        callback();
+      });
+    }
+  ], function(err) {
+      if (err) {
+        reply.status = false;
+        response.send(reply);
+        throw err;
+      }
+      // finally update the new data into the businesses table
+      var updateInfo = 'UPDATE businesses SET number_of_reviews = ?, average_rating = ? WHERE business_id = ?';
+      con.query(updateInfo, [numberOfReviews, averageRating, businessId], function(err, result) {
+        if(err) {
+          reply.status = false;
+          response.send(reply);
+          throw err;
+        }
+        console.log("Data updated");
+        response.send(reply);
+      });
+  });
+});
+
+app.listen(3000, function() {
+  console.log("AppIt Web Server is running on port 3000 ...");
+});
